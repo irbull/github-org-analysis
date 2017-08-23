a9e413c61d61ba70ec4b6df32c345f36e1dc2377
Ian Bull <irbull@eclipsesource.com>
2017-08-22 22:42:49 -0700

    Use pushed_at for the last update time
    updated_at doesn't appear to show the last time a repo was updated.
    Instead, we now use the last time a code push happened.

--- load_all_org_private_repos.js
+++ load_all_org_private_repos.js
@@ -34,15 +34,15 @@ function resolveAllPrivateRepos(res, resolve, reject) {
     for (let repo of repos) {
         promises.push(new Promise((resolve, reject) => {
             github.repos.getCommit({ 'owner': settings.organization, 'repo': repo.name, 'sha': repo.default_branch }, ((err, commit) => {
-                if ( err) console.log(err);
-                if ( commit && commit.data && commit.data.author) {
-                  resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.updated_at}, ${repo.default_branch}, ${commit.data.author.login}`)
-                } else if ( commit && commit.data && commit.data.committer) {
-                  resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.updated_at}, ${repo.default_branch}, ${commit.data.committer.login}`)
-                } else if ( commit && commit.data && commit.data.commit && commit.data.commit.committer ) {
-                  resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.updated_at}, ${repo.default_branch}, ${commit.data.commit.committer.email}`)
+                if (err) console.log(err);
+                if (commit && commit.data && commit.data.author) {
+                    resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.pushed_at}, ${repo.default_branch}, ${commit.data.author.login}`)
+                } else if (commit && commit.data && commit.data.committer) {
+                    resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.pushed_at}, ${repo.default_branch}, ${commit.data.committer.login}`)
+                } else if (commit && commit.data && commit.data.commit && commit.data.commit.committer) {
+                    resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.pushed_at}, ${repo.default_branch}, ${commit.data.commit.committer.email}`)
                 } else {
-                  resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.updated_at}, ${repo.default_branch}, undefined`)
+                    resolve(`${repo.name}, ${repo.full_name}, ${repo.html_url}, ${repo.pushed_at}, ${repo.default_branch}, undefined`)
                 }
             }));
         }));
